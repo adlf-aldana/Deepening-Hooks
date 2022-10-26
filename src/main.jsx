ef7ed9124fbdd302795025641e43cbb989133d19
Adolfo <adlf.aldana@gmail.com>
2022-10-26 18:44:10 -0400

    #feat: useCounter - customHook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HookApp } from "./HookApp";
+// import { HookApp } from "./HookApp";
+// import { CounterApp } from "./Components/CounterApp";
+import { CounterWithCustomHook } from "./Components/CounterWithCustomHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HookApp />
+    {/* <HookApp /> */}
+    {/* <CounterApp /> */}
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
