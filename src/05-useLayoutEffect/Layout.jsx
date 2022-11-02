0f8fd30511fa13ad03c14692f85121e41257279a
Adolfo <adlf.aldana@gmail.com>
2022-11-01 21:05:06 -0400

    #feat: useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,37 @@
+import { useCounter, useFetch } from "../hooks/index";
+import { LoadingQuote, Quote } from "../03-examples";
+
+export const Layout = () => {
+  const { increment, counter } = useCounter(1);
+
+  const { data, isLoading, hasError } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
+  );
+
+  const { author, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {/* {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer"> {author} </footer>
+        </blockquote>
+      )} */}
+      {isLoading ? <LoadingQuote /> : <Quote quote={quote} author={author} />}
+
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={() => increment()}
+      >
+        Next quote
+      </button>
+    </>
+  );
+};
